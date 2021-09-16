1f5c54ff9e684e36639719d8c05a47db5e800222
Kierán Meinhardt <kmein@posteo.de>
2021-09-16 23:34:49 +0200

    feat(default.nix): add static html pages and stats script to output

--- default.nix
+++ default.nix
@@ -1,6 +1,11 @@
-{ lib, rustPlatform }:
+{ lib, rustPlatform, jq }:
 rustPlatform.buildRustPackage rec {
   name = "tinc-graph";
   src = ./.;
+  dontPatchShebangs = 1;
+  postInstall = ''
+    cp -r $src/static $out
+    cp $src/tinc-statistics $out/bin
+  '';
   cargoSha256 = "1v8fydpqgcv52fcvkpw6j9cx2xhdryn98b91f3d53ph2qqxj25qn";
 }
